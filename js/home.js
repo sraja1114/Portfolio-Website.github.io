8adff10e7bf0e013207d0333d8dd8ef9e02ac979
sraja1114 <raja.sohaib7743@gmail.com>
2022-07-01 23:32:10 -0500

    disappear and bouncing arrow

--- js/home.js
+++ js/home.js
@@ -1,6 +1,18 @@
 function scrolltoInfo() {
     $("#downArrow").click(function() {console.log("clicked");
         $('html,body').animate({
-            scrollTop: $("#info").offset().top}, 475);
+            scrollTop: $("#info").offset().top}, 400, 'linear');
     });
 }
+
+$(document).ready(function() {
+  
+    $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
+      if (scroll >= 1) {
+        $('.arrow').addClass('fade');
+      } else{
+        $('.arrow').removeClass('fade');
+      }
+    })
+  });

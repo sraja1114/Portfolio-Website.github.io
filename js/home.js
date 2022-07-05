c2887b879f4123e71b44bc5ee1b9469b2d5b22a9
sraja1114 <raja.sohaib7743@gmail.com>
2022-07-04 19:03:40 -0500

    change8

--- js/home.js
+++ js/home.js
@@ -1,67 +1,81 @@
-var $hb = $('html, body');  
-$('#downArrow').click(function() {
-  $hb.animate({ 
-      scrollTop: $("#info").offset().top // set scrollTarget to your desired position
-  }, 400, "linear");
+var $hb = $("html, body");
+$("#downArrow").click(function () {
+  if (window.innerWidth > 600) {
+    $hb.animate(
+      {
+        scrollTop: $("#info").offset().top, // set scrollTarget to your desired position
+      },
+      400,
+      "linear"
+    );
+    console.log("desktop");
+  } else {
+    $hb.animate(
+      {
+        scrollTop: $(".tamu").offset().top, // set scrollTarget to your desired position
+      },
+      400,
+      "linear"
+    );
+    console.log("mobile");
+  }
 });
 
-console.log("change7")
+console.log("change8");
 
-  // Stop the animation if the user scrolls. Defaults on .stop() should be fine
-$hb.bind("scroll mousedown DOMMouseScroll mousewheel keyup", function(e){
-  if ( e.which > 0 || e.type === "mousedown" || e.type === "mousewheel"){
-      $hb.stop().unbind('scroll mousedown DOMMouseScroll mousewheel keyup'); // This identifies the scroll as a user action, stops the animation, then unbinds the event straight after (optional)
+// Stop the animation if the user scrolls. Defaults on .stop() should be fine
+$hb.bind("scroll mousedown DOMMouseScroll mousewheel keyup", function (e) {
+  if (e.which > 0 || e.type === "mousedown" || e.type === "mousewheel") {
+    $hb.stop().unbind("scroll mousedown DOMMouseScroll mousewheel keyup"); // This identifies the scroll as a user action, stops the animation, then unbinds the event straight after (optional)
   }
-}); 
+});
 
-$(document).ready(function() {
-  
-    $(window).scroll(function() {
-      var scroll = $(window).scrollTop();
-      if (scroll >= 200) {
-        $('.arrow').addClass('fade');
-      } else{
-        $('.arrow').removeClass('fade');
-      }
-    })
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 200) {
+      $(".arrow").addClass("fade");
+    } else {
+      $(".arrow").removeClass("fade");
+    }
   });
+});
 
-$(document).ready(function() {
-  
-    $(window).scroll(function() {
-      var scroll = $(window).scrollTop();
-      if (scroll >= 200) {
-        $('.arrow').addClass('fade');
-      } else{
-        $('.arrow').removeClass('fade');
-      }
-    })
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 200) {
+      $(".arrow").addClass("fade");
+    } else {
+      $(".arrow").removeClass("fade");
+    }
   });
+});
 
-
-function set100vhVar(){
+function set100vhVar() {
   // If less than most tablets, set CSS var to window height.
   let value = "96.5vh";
 
   // If window size is iPad or smaller, then use JS to set screen height.
-  if (window.innerWidth && window.innerWidth <= 1024) { window.outer
-      // Navbar in pixels
-      var navbarHeight = document.getElementById('navbar').clientHeight;;
+  if (window.innerWidth && window.innerWidth <= 1024) {
+    window.outer;
+    // Navbar in pixels
+    var navbarHeight = document.getElementById("navbar").clientHeight;
 
-      //Subtract the height of the navbar from the inner window height.
-      var heightWoNavbar = window.innerHeight - navbarHeight;
-      value = heightWoNavbar.toString() + `px`;
+    //Subtract the height of the navbar from the inner window height.
+    var heightWoNavbar = window.innerHeight - navbarHeight;
+    value = heightWoNavbar.toString() + `px`;
   }
   document.documentElement.style.setProperty("--real100vh", value);
 }
 set100vhVar();
 
-function set100vwVar(){
+function set100vwVar() {
   // If less than most tablets, set CSS var to window height.
   let value = "100vw";
- 
+
   //sets width based on navbar width
-  var navbarWidth = document.getElementById('navbar').clientWidth;
+  var navbarWidth = document.getElementById("navbar").clientWidth;
   value = navbarWidth.toString() + `px`;
   document.documentElement.style.setProperty("--real100vw", value);
 }

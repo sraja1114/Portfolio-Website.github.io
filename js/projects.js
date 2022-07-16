f558a2a67bf8d73ef3a95d2b12cccc77abc241c4
sraja1114 <raja.sohaib7743@gmail.com>
2022-07-15 23:03:14 -0500

    projects added

---
+++ js/projects.js
@@ -0,0 +1,11 @@
+function set100vwVar() {
+    // If less than most tablets, set CSS var to window height.
+    let value = "100vw";
+  
+    //sets width based on navbar width
+    var navbarWidth = document.getElementById("navbar").clientWidth;
+    value = navbarWidth.toString() + `px`;
+    document.documentElement.style.setProperty("--real100vw", value);
+  }
+  window.onresize = set100vwVar;
+  window.onload = set100vwVar;
